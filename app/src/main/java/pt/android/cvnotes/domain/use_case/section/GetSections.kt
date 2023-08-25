2be144e5de182a0d731da616204b317e00aed632
Rui Vieira <revs87@msn.com>
2023-08-25 01:13:44 +0100

    Update GetSections.kt

--- app/src/main/java/pt/android/cvnotes/domain/use_case/section/GetSections.kt
+++ app/src/main/java/pt/android/cvnotes/domain/use_case/section/GetSections.kt
@@ -1,22 +1,13 @@
 package pt.android.cvnotes.domain.use_case.note
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import pt.android.cvnotes.domain.model.Section
 import pt.android.cvnotes.domain.repository.SectionRepository
-import pt.android.cvnotes.domain.util.SectionType
 
 class GetSections(
     private val sectionRepository: SectionRepository
 ) {
-    operator fun invoke(
-        section: SectionType = SectionType.ALL
-    ): Flow<List<Section>> {
-        return sectionRepository.getSections().map { sections ->
-            when (section) {
-                SectionType.ALL -> sections
-                else -> sections.filter { section.id == it.type }
-            }
-        }
+    operator fun invoke(): Flow<List<Section>> {
+        return sectionRepository.getSections()
     }
 }
