625813159bc018c83c4d257745ae338d2d516037
Rui Vieira <revs87@msn.com>
2023-09-05 18:49:05 +0100

    Cleansing

--- app/src/main/java/pt/android/cvnotes/ui/editnote/EditNoteScreen.kt
+++ app/src/main/java/pt/android/cvnotes/ui/editnote/EditNoteScreen.kt
@@ -42,7 +42,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import pt.android.cvnotes.domain.model.Note
 import pt.android.cvnotes.domain.model.asString
