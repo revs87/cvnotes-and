8dcfe4ac0badf7562b3f5692445d36390de8cbed
Rui Vieira <revs87@msn.com>
2023-09-10 18:04:03 +0100

    JUnit5 support + readability improvements

--- app/src/test/java/pt/rvcoding/cvnotes/ExampleUnitTest.kt
+++ app/src/test/java/pt/rvcoding/cvnotes/ExampleUnitTest.kt
@@ -1,7 +1,10 @@
 package pt.rvcoding.cvnotes
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -9,8 +12,15 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    @BeforeEach
+    fun setUp() {}
+
+    @AfterEach
+    fun tearDown() {}
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertThat(2 + 2).isEqualTo(4)
     }
 }
